5e2fa6f8824899bcaf3c7fa44b754abbb944ff26
Piyush Jha <95862487+pandit27@users.noreply.github.com>
2025-02-17 16:22:45 +0530

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,15 +1,15 @@
 module.exports = (bot) => {
     bot.onText(/\/start/, (msg) => {
-        bot.sendMessage(msg.chat.id, "👋 Hello! I am your reminder bot made by @PV_027.\nUse /days to check the exam countdown.");
+        bot.sendMessage(msg.chat.id, "👋 Hello! I am your reminder bot made by @PV_027.\nUse /days to check the CUET PG exam countdown.");
     });
     
     bot.onText(/\/days/, (msg) => {
         const daysLeft = Math.ceil((EXAM_DATE - new Date()) / (1000 * 60 * 60 * 24));
-        bot.sendMessage(msg.chat.id, `📆 ${daysLeft} days left until the CUET PG exam! Stay prepared! 💪`);
+        bot.sendMessage(msg.chat.id, `📆 ${daysLeft} days left until the CUET PG exam! Keep grinding.`);
     });
     
     bot.onText(/\/help/, (msg) => {
-        bot.sendMessage(msg.chat.id, "📌 contact: @PV_027");
+        bot.sendMessage(msg.chat.id, "📌 commands: \n/start : to start the bot. \n/exam : to get CUET PG exam countdown. \n//nimcet	to get NIMCET exam countdown. \n contact : @PV_027");
     });
 
     bot.onText(/\/nimcet/, (msg) => {
