19c873cbcea2a32fcfdd9deb7e9c024fbf729e1f
Piyush Jha <95862487+pandit27@users.noreply.github.com>
2025-02-17 10:57:28 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,32 @@
+const TelegramBot = require("node-telegram-bot-api");
+const TOKEN = "8169135424:AAFMNrthUWEsFMAE3qQJSuSCyv9rJxNg9jI";
+const CHAT_ID = "-1002165186773";
+const EXAM_DATE = new Date("2025-03-10");
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+// remainder function
+const sendReminder = () => {
+    const today = new Date();
+    const timeDiff = EXAM_DATE - today;
+    const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+    let message;
+
+    if (daysLeft > 0) {
+        message = `📢 <b>CUET PG Exam Reminder!</b>\n\n⏳ Only <b>${daysLeft} days</b> left! \n\nStay focused and keep grinding! 🚀`;
+    } 
+    else if (daysLeft === 0) message = `🚨 <b>Today is the Exam!</b> 🎯\n\nBest of luck! 🍀`;
+    else message = `✅ <b>Exam Completed!</b> 🎉\nHope you did well!`;
+
+    bot.sendMessage(CHAT_ID, message, { parse_mode: "HTML" }).catch(error => {
+        console.log("Error sending message:", error.response.body);
+    });
+}
+
+// to send remainder
+sendReminder();
+
+// function to schedule at a certain time
+setInterval(() => {
+    const now = new Date();
+    if (now.getHours() === 9 && now.getMinutes() === 0) sendReminder();}, 60 * 1000);
