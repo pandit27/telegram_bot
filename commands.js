5e8db428e5b2bfd604abc42bcc5005a17b1fa556
Piyush Jha <95862487+pandit27@users.noreply.github.com>
2025-02-17 17:30:58 +0530

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,14 +1,14 @@
-// Import the quiz questions
-const qs = require('./dsa_quiz');
-
 module.exports = (bot) => {
     bot.onText(/\/start/, (msg) => {
         bot.sendMessage(msg.chat.id, "👋 Hello! I am your reminder bot made by @PV_027.\nUse /days to check the CUET PG exam countdown.");
     });
     
     bot.onText(/\/days/, (msg) => {
-        const daysLeft = Math.ceil((EXAM_DATE - new Date()) / (1000 * 60 * 60 * 24));
+        const EXAM_DATE = new Date("2025-03-10");
+        const now = new Date();
+        const daysLeft = Math.ceil((EXAM_DATE - now) / (1000 * 60 * 60 * 24));
         bot.sendMessage(msg.chat.id, `📆 ${daysLeft} days left until the CUET PG exam! Keep grinding.`);
+        console.log(daysLeft);
     });
     
     bot.onText(/\/help/, (msg) => {
