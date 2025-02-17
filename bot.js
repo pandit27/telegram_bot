e17b7ecd4ee294961f8450cee701ad98864596e7
Piyush Jha <95862487+pandit27@users.noreply.github.com>
2025-02-17 15:55:59 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,17 +31,3 @@ sendReminder();
 setInterval(() => {
     const now = new Date();
     if (now.getHours() === 9 && now.getMinutes() === 0) sendReminder();}, 60 * 1000);
-
-// some commands
-// bot.onText(/\/start/, (msg) => {
-//     bot.sendMessage(msg.chat.id, "👋 Hello! I am your reminder bot made by @PV_027.\nUse /days to check the exam countdown.");
-// });
-
-// bot.onText(/\/days/, (msg) => {
-//     const daysLeft = Math.ceil((EXAM_DATE - new Date()) / (1000 * 60 * 60 * 24));
-//     bot.sendMessage(msg.chat.id, `📆 ${daysLeft} days left until the CUET PG exam! Keep grinding.`);
-// });
-
-// bot.onText(/\/help/, (msg) => {
-//     bot.sendMessage(msg.chat.id, "📌 contact: @PV_027");
-// });
