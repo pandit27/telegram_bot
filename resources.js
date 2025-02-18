d525b875203e2c5c60fd85377f7113454871fb0a
Priyanshu Jha <priyanshujha131@gmail.com>
2025-02-18 16:48:39 +0530

    updated

--- resources.js
+++ resources.js
@@ -20,12 +20,12 @@ module.exports = (bot) => {
 
         const message = `📚 *NIMCET 2025 Resources* \n\n` +
             `🧑‍🏫 *Math:* \n
-            | [📖 Book](https://t.me/nimcet_resources/87) | 
-            | [📝 DPP](https://t.me/nimcet_resources/587) | 
-            | [📂 PYQs](https://t.me/nimcet_resources/847) | \n\n` +
+             [📖 Book](https://t.me/nimcet_resources/87) 
+             [📝 DPP](https://t.me/nimcet_resources/587) 
+             [📂 PYQs](https://t.me/nimcet_resources/847) \n\n` +
             `👩‍🏫 *Reasoning:* \n
-            | [📖 Book](https://t.me/nimcet_resources/571) | 
-            | [📝 DPP](https://t.me/nimcet_resources/876) |\n\n` +
+             [📖 Book](https://t.me/nimcet_resources/571) 
+             [📝 DPP](https://t.me/nimcet_resources/876) \n\n` +
             `💻 *Groups & Channels* \n
             1. [NIMCET resources](https://t.me/nimcet_resources)
             2. [NIMCET group 2025](https://t.me/nimcet2025_group) 
