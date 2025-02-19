6212b4b217bf05bfd67b5d8f9fa2e3cf92be7451
Priyanshu Jha <priyanshujha131@gmail.com>
2025-02-19 13:37:59 +0530

    test update

---
+++ questions/math.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        question: "What is the value of π (pi) approximately?",
+        options: ["3.14", "2.71", "1.61", "4.13"],
+        answer: "3.14"
+    },
+    {
+        question: "What is the derivative of x^2?",
+        options: ["2x", "x", "x^2", "0"],
+        answer: "2x"
+    }
+];
---
+++ questions/reasoning.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        question: "What comes next in the sequence: 2, 6, 12, 20, __?",
+        options: ["28", "30", "32", "34"],
+        answer: "30"
+    },
+    {
+        question: "If all roses are flowers and some flowers fade quickly, then:",
+        options: ["All roses fade quickly", "Some roses fade quickly", "Some flowers do not fade quickly", "None of the above"],
+        answer: "Some flowers do not fade quickly"
+    }
+];
