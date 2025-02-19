59d7de6f3ad3a58961928388e8446f0d1722bb2a
Priyanshu Jha <priyanshujha131@gmail.com>
2025-02-19 13:41:11 +0530

    test 1

--- dsa_quiz.js
+++ dsa_quiz.js
@@ -1,30 +1,25 @@
-// importing questions
+// Importing questions
 const mathQuestions = require("./questions/math");
 const reasoningQuestions = require("./questions/reasoning");
 const dsaQuestions = require("./questions/dsa");
 
+// Mapping quiz categories correctly
+const quizQuestions = {
+    Math: mathQuestions,
+    Reasoning: reasoningQuestions,
+    DSA: dsaQuestions
+};
+
 module.exports = (bot) => {
-    // const quizQuestions = [
-        
-    // ];
-    
-
-
-
-    // owner chat_id
     const ownerChatId = '5036581553';
 
-
-  
-    
-    // send poll
+    // Send category selection buttons
     bot.onText(/\/quiz/, (msg) => {
         const chatId = msg.chat.id;
         const userId = msg.from.id;
-    
-        // log the chat type
+
         console.log("chat type:", msg.chat.type);
-    
+
         if (msg.chat.type === "private") {
             bot.sendMessage(chatId, "Select a quiz category:", {
                 reply_markup: {
@@ -38,8 +33,9 @@ module.exports = (bot) => {
         } 
         else if (msg.chat.type === "group" || msg.chat.type === "supergroup") {
             if (String(userId) === String(ownerChatId)) {
-                const randomQuestion = quizQuestions[Math.floor(Math.random() * quizQuestions.length)];
-    
+                const allQuestions = [...mathQuestions, ...reasoningQuestions, ...dsaQuestions]; // Combine all questions
+                const randomQuestion = allQuestions[Math.floor(Math.random() * allQuestions.length)];
+
                 bot.sendPoll(chatId, randomQuestion.question, randomQuestion.options, {
                     is_anonymous: false,
                     type: 'quiz',
@@ -57,9 +53,13 @@ module.exports = (bot) => {
         const chatId = query.message.chat.id;
         const selectedCategory = query.data.split("_")[1]; // Extract category name
 
+        console.log("Selected category:", selectedCategory); // Debugging log
+
         if (quizQuestions[selectedCategory]) {
-            // Randomly pick a question
+            // Pick a random question from the selected category
             const randomQuestion = quizQuestions[selectedCategory][Math.floor(Math.random() * quizQuestions[selectedCategory].length)];
+
+            console.log("Selected question:", randomQuestion); // Debugging log
 
             // Send quiz poll
             bot.sendPoll(chatId, randomQuestion.question, randomQuestion.options, {
@@ -70,7 +70,8 @@ module.exports = (bot) => {
 
             // Acknowledge callback to remove "loading" icon
             bot.answerCallbackQuery(query.id);
+        } else {
+            bot.answerCallbackQuery(query.id, { text: "Error: Category not found!", show_alert: true });
         }
     });
-    
 };
