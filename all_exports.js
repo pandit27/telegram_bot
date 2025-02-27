8e9d52be931705b8fa4202ac06d65f782e644b94
Priyanshu Jha <priyanshujha131@gmail.com>
2025-02-27 19:40:54 +0530

    updated

--- all_exports.js
+++ all_exports.js
@@ -4,12 +4,6 @@ module.exports = (bot, CHAT_ID, TEST_ID, OWNER_ID) => {
     const faq_chats = require("./client/group tools/group chat/faq_chat");
     faq_chats(bot, CHAT_ID);
     /* ------------------------------------------------------------------------------------ */
-    
-    
-    /* ------------------------------------------------------------------------------------ */
-    // const message_analyzer = require("./client/admin tools/private chat/message_analyzer");
-    // message_analyzer(bot, OWNER_ID, TEST_ID);
-    /* ------------------------------------------------------------------------------------ */
 
 
     /* ------------------------------------------------------------------------------------ */
