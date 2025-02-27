66c449a9322d4edcc8ca555d03add908bf80b0f6
Priyanshu Jha <priyanshujha131@gmail.com>
2025-02-27 18:22:17 +0530

    updated

---
+++ client/scripts/git_add.sh
@@ -0,0 +1,5 @@
+# add all changes
+git add .
+
+# print a message in the console (add to git log)
+echo "git added successfully" gitpush.sh
---
+++ client/scripts/git_commit.sh
@@ -0,0 +1,9 @@
+# add all changes
+git add .
+
+# add a commit message
+commit_message="updated"
+git commit -m "$commit_message"
+
+# print a message in the console (changes commited in git log)
+echo "changes commited successfully" gitpush.sh
