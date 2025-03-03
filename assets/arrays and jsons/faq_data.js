6ad4f527b50520d7d319be514ba904e06156056e
Priyanshu Jha <priyanshujha131@gmail.com>
2025-03-03 16:36:42 +0530

    updated

--- assets/arrays and jsons/faq_data.js
+++ assets/arrays and jsons/faq_data.js
@@ -1,5 +1,5 @@
 const faq = [
-    { keywords: ["admin", "owner", "spam", "/spam", "/admin"], response: "Reported to admin! Contact @PV_027" },
+    { keywords: ["owner", "spam", "/spam", "/admin"], response: "Reported to admin! Contact @PV_027" },
     { keywords: ["nimcet date"], response: "The date of NIMCET is yet to be announced." },
 ];
 
