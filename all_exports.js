4a85c614b704c257aad76f03caeae130e7c6c8b9
Piyush Jha <95862487+pandit27@users.noreply.github.com>
2025-04-28 21:21:56 +0530

    Update all_exports.js

--- all_exports.js
+++ all_exports.js
@@ -3,6 +3,11 @@ module.exports = (bot, CHAT_ID, TEST_ID, OWNER_ID) => {
     /* ------------------------------------------------------------------------------------ */
     const faq_chats = require("./client/group tools/group chat/faq_chat");
     faq_chats(bot, CHAT_ID);
+
+
+
+    const rapid_quiz = require("./client/group tools/rapid_quiz");
+    rapid_quiz(bot, OWNER_ID, CHAT_ID);
     /* ------------------------------------------------------------------------------------ */
 
 
